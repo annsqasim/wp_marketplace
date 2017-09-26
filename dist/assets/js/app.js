14ef4407d9eeafe652e99782b4a27ee37f839fa3
Qasim <muhammed.bin-qasim@vistajet.com>
2017-09-26 23:47:43 +0500

    file changes for assets and JS

--- dist/assets/js/app.js
+++ dist/assets/js/app.js
@@ -14491,6 +14491,49 @@ window.$ = _jquery2.default;
 
 
 (0, _jquery2.default)(document).foundation();
+(0, _jquery2.default)(function () {
+  var h = (0, _jquery2.default)('#responsive-menu').height();
+
+  (0, _jquery2.default)(window).scroll(function () {
+    if ((0, _jquery2.default)(window).scrollTop() > h) {
+      (0, _jquery2.default)('#responsive-menu').addClass('navbar-fixed');
+    } else {
+      (0, _jquery2.default)('#responsive-menu').removeClass('navbar-fixed');
+    }
+  });
+
+  // Select all links with hashes
+  (0, _jquery2.default)('a[href*="#"]')
+  // Remove links that don't actually link to anything
+  .not('[href="#"]').not('[href="#0"]').click(function (event) {
+    // On-page links
+    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+      // Figure out element to scroll to
+      var target = (0, _jquery2.default)(this.hash);
+      target = target.length ? target : (0, _jquery2.default)('[name=' + this.hash.slice(1) + ']');
+      // Does a scroll target exist?
+      if (target.length) {
+        // Only prevent default if animation is actually gonna happen
+        event.preventDefault();
+        (0, _jquery2.default)('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000, function () {
+          // Callback after animation
+          // Must change focus!
+          var $target = (0, _jquery2.default)(target);
+          $target.focus();
+          if ($target.is(":focus")) {
+            // Checking if the target was focused
+            return false;
+          } else {
+            $target.attr('tabindex', '-1'); // Adding tabindex for elements not focusable
+            $target.focus(); // Set focus again
+          };
+        });
+      }
+    }
+  });
+});
 
 /***/ }),
 /* 21 */
